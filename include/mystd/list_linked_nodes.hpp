3b5a3fd092aad70401f904478cc366936ddbffbe
kirillin <kaartemov@gmail.com>
2025-07-29 18:34:18 +0300

    Add doubly linked list

--- include/mystd/list_linked_nodes.hpp
+++ include/mystd/list_linked_nodes.hpp
@@ -1,17 +1,18 @@
 #pragma once
 
-#include <array>
-#include <type_traits>
+#include <initializer_list>
+#include <iterator>
+#include <stdexcept>
+#include <utility>
 
-namespace my::arraybased {
+namespace my::heapbased {
 
-/// @brief comments
-/// @tparam T 
+/// @brief doubly linked list on linked nodes
 template <typename T>
 class list {
 public:
     using value_type = T;
-    using reference  = T&;
+    using reference = T&;
     using const_reference = const T&;
     using pointer = T*;
     using const_pointer = const T*;
@@ -19,27 +20,236 @@ public:
 
 private:
     struct Node {
-        value_type data;
-        Node* prev;
-        Node* next;
-
-        Node(const value_data& data, Node* prev = nullptr, Node* next = nullptr) : data(data), prev(prev), next(next) {}
+        value_type data{};
+        Node* prev = nullptr;
+        Node* next = nullptr;
     };
-    
-    Node* head = nullptr;
 
-private:
+    Node* head_ = nullptr;
+    Node* tail_ = nullptr;
+    size_type size_ = 0;
+
+    // iterator implementation
     template <bool IsConst>
-    class iterator_basic {}
+    class iterator_basic {
+    public:
+        using node_type = std::conditional_t<IsConst, const Node, Node>;
+        using list_type = std::conditional_t<IsConst, const list, list>;
+        using reference = std::conditional_t<IsConst, const T&, T&>;
+        using pointer = std::conditional_t<IsConst, const T*, T*>;
+        using difference_type = std::ptrdiff_t;
+        using iterator_category = std::bidirectional_iterator_tag;
+
+        iterator_basic() = default;
+        explicit iterator_basic(list_type* owner, node_type* ptr = nullptr)
+            : owner_(owner), ptr_(ptr) {}
+
+        node_type* node() const { return ptr_; }
+
+        reference operator*() const {
+            if (!ptr_) throw std::out_of_range("Dereferencing end() iterator");
+            return ptr_->data;
+        }
+
+        pointer operator->() const {
+            if (!ptr_) throw std::out_of_range("Accessing through end() iterator");
+            return &ptr_->data;
+        }
+
+        iterator_basic& operator++() {
+            if (ptr_) ptr_ = ptr_->next;
+            return *this;
+        }
+
+        iterator_basic& operator--() {
+            if (!ptr_) {
+                if (!owner_ || !owner_->tail_)
+                    throw std::out_of_range("Cannot decrement end() of empty list");
+                ptr_ = owner_->tail_;
+            } else {
+                ptr_ = ptr_->prev;
+            }
+            return *this;
+        }
+
+        iterator_basic operator++(int) { auto tmp = *this; ++(*this); return tmp; }
+        iterator_basic operator--(int) { auto tmp = *this; --(*this); return tmp; }
+
+        bool operator==(const iterator_basic& rhs) const { return ptr_ == rhs.ptr_; }
+        bool operator!=(const iterator_basic& rhs) const { return ptr_ != rhs.ptr_; }
+
+    private:
+        list_type* owner_ = nullptr;
+        node_type* ptr_ = nullptr;
+    };
+
+    void free_nodes(Node* node) {
+        while (node) {
+            Node* next = node->next;
+            delete node;
+            node = next;
+        }
+        head_ = tail_ = nullptr;
+        size_ = 0;
+    }
 
 public:
     using iterator = iterator_basic<false>;
     using const_iterator = iterator_basic<true>;
 
-public:
+    // ctor
+    list() = default;
 
-    
+    list(std::initializer_list<value_type> init) {
+        for (const auto& val : init) {
+            push_back(val);
+        }
+    }
 
+    // copy
+    list(const list& other) {
+        for (const auto& val : other) {
+            push_back(val);
+        }
+    }
+
+    list& operator=(const list& other) {
+        if (this != &other) {
+            list tmp(other);
+            swap(tmp);
+        }
+        return *this;
+    }
+
+    // move
+    list(list&& other) noexcept
+        : head_(std::exchange(other.head_, nullptr)),
+          tail_(std::exchange(other.tail_, nullptr)),
+          size_(std::exchange(other.size_, 0)) {}
+
+    list& operator=(list&& other) noexcept {
+        if (this != &other) {
+            clear();
+            head_ = std::exchange(other.head_, nullptr);
+            tail_ = std::exchange(other.tail_, nullptr);
+            size_ = std::exchange(other.size_, 0);
+        }
+        return *this;
+    }
+
+    ~list() {
+        clear();
+    }
+
+    // element access
+    reference front() {
+        if (!head_) throw std::out_of_range("front() on empty list");
+        return head_->data;
+    }
+
+    const_reference front() const {
+        if (!head_) throw std::out_of_range("front() on empty list");
+        return head_->data;
+    }
+
+    reference back() {
+        if (!tail_) throw std::out_of_range("back() on empty list");
+        return tail_->data;
+    }
+
+    const_reference back() const {
+        if (!tail_) throw std::out_of_range("back() on empty list");
+        return tail_->data;
+    }
+
+    // iterator access
+    iterator begin() noexcept { return iterator(this, head_); }
+    iterator end() noexcept { return iterator(this, nullptr); }
+
+    const_iterator begin() const noexcept { return const_iterator(this, head_); }
+    const_iterator end() const noexcept { return const_iterator(this, nullptr); }
+
+    const_iterator cbegin() const noexcept { return const_iterator(this, head_); }
+    const_iterator cend() const noexcept { return const_iterator(this, nullptr); }
+
+    // capacity
+    bool empty() const noexcept { return size_ == 0; }
+    size_type size() const noexcept { return size_; }
+
+    // modifiers
+    iterator insert(iterator pos, const_reference value) {
+        Node* curr = pos.node();
+        Node* new_node = new Node{value};
+
+        if (!curr) { // insert at end
+            new_node->prev = tail_;
+            if (tail_) tail_->next = new_node;
+            tail_ = new_node;
+            if (!head_) head_ = new_node;
+        } else {
+            new_node->next = curr;
+            new_node->prev = curr->prev;
+            if (curr->prev) curr->prev->next = new_node;
+            else head_ = new_node;
+            curr->prev = new_node;
+        }
+
+        ++size_;
+        return iterator(this, new_node);
+    }
+
+    iterator insert(const_iterator pos, const_reference value) {
+        return insert(iterator(const_cast<Node*>(pos.node())), value);
+    }
+
+    iterator erase(iterator pos) {
+        Node* node = pos.node();
+        if (!node) throw std::out_of_range("Cannot erase end() iterator");
+
+        Node* prev = node->prev;
+        Node* next = node->next;
+
+        if (prev) prev->next = next;
+        else head_ = next;
+
+        if (next) next->prev = prev;
+        else tail_ = prev;
+
+        delete node;
+        --size_;
+
+        return iterator(this, next);
+    }
+
+    void push_front(const_reference value) {
+        insert(begin(), value);
+    }
+
+    void push_back(const_reference value) {
+        insert(end(), value);
+    }
+
+    void pop_front() {
+        if (empty()) throw std::out_of_range("pop_front() on empty list");
+        erase(begin());
+    }
+
+    void pop_back() {
+        if (empty()) throw std::out_of_range("pop_back() on empty list");
+        iterator tmp = end();
+        --tmp;
+        erase(tmp);
+    }
+
+    void clear() {
+        free_nodes(head_);
+    }
+
+    void swap(list& other) noexcept {
+        std::swap(head_, other.head_);
+        std::swap(tail_, other.tail_);
+        std::swap(size_, other.size_);
+    }
 };
 
 } // namespace my::arraybased
---
+++ tests/unit_tests/test_list_linked_nodes.cpp
@@ -0,0 +1,121 @@
+#include <gtest/gtest.h>
+#include "mystd/list_linked_nodes.hpp"
+
+namespace my::heapbased::testing {
+
+TEST(ListTest, DefaultInitialization) {
+    list<int> a;
+    EXPECT_TRUE(a.empty());
+    EXPECT_EQ(a.size(), 0);
+}
+
+TEST(ListTest, InitializerListConstruction) {
+    list<int> a = {10, 20, 30};
+    EXPECT_FALSE(a.empty());
+    EXPECT_EQ(a.size(), 3);
+    EXPECT_EQ(a.front(), 10);
+    EXPECT_EQ(a.back(), 30);
+}
+
+TEST(ListTest, PushFrontBackAndPopFrontBack) {
+    list<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_front(0);
+    EXPECT_EQ(a.front(), 0);
+    EXPECT_EQ(a.back(), 2);
+
+    a.pop_front();
+    EXPECT_EQ(a.front(), 1);
+
+    a.pop_back();
+    EXPECT_EQ(a.back(), 1);
+
+    a.pop_back();
+    EXPECT_TRUE(a.empty());
+}
+
+TEST(ListTest, ForwardIteratorTraversal) {
+    list<int> a = {1, 2, 3, 4};
+    int expected = 1;
+    for (auto it = a.begin(), ite = a.end(); it != ite; ++it) {
+        EXPECT_EQ(*it, expected++);
+    }
+}
+
+TEST(ListTest, ReverseIteratorTraversal) {
+    list<int> a = {1, 2, 3, 4};
+    auto it = a.end();
+    int expected = 4;
+    while (it != a.begin()) {
+        --it;
+        EXPECT_EQ(*it, expected--);
+    }
+}
+
+TEST(ListTest, InsertBeginMiddleEnd) {
+    list<int> a = {1, 3};
+
+    a.insert(a.begin(), 0);        // {0, 1, 3}
+    auto it = a.begin();
+    ++it;                          // it -> 1
+    ++it;
+    a.insert(it, 2);               // {0, 1, 2, 3} inser before 3
+    a.insert(a.end(), 4);          // {0, 1, 2, 3, 4}
+
+    int expected = 0;
+    for (auto& val : a) {
+        EXPECT_EQ(val, expected++);
+    }
+}
+
+TEST(ListTest, EraseBeginMiddleEnd) {
+    list<int> a = {0, 1, 2, 3, 4};
+
+    a.erase(a.begin());             // erase 0
+    auto it = a.begin();
+    ++it;                           // it -> 2
+    a.erase(it);                    // erase 2
+    it = a.end(); --it;             // it -> 4
+    a.erase(it);                    // erase 4
+
+    std::vector<int> expected = {1, 3};
+    auto exp_it = expected.begin();
+    for (auto& val : a) {
+        EXPECT_EQ(val, *exp_it++);
+    }
+}
+
+TEST(ListTest, CopyConstructorAndAssignment) {
+    list<int> a = {1, 2, 3};
+    list<int> b = a;               // copy constructor
+    list<int> c; c = a;            // copy assignment
+
+    EXPECT_EQ(a.front(), b.front());
+    EXPECT_EQ(a.front(), c.front());
+    EXPECT_EQ(a.back(), b.back());
+    EXPECT_EQ(a.back(), c.back());
+
+}
+
+TEST(ListTest, MoveConstructorAndAssignment) {
+    list<int> a = {1, 2, 3};
+    list<int> b = std::move(a);
+    EXPECT_EQ(b.size(), 3);
+    EXPECT_TRUE(a.empty());
+
+    list<int> c;
+    c = std::move(b);
+    EXPECT_EQ(c.size(), 3);
+    EXPECT_TRUE(b.empty());
+}
+
+TEST(ListTest, ConstIterator) {
+    const list<int> a = {5, 6, 7};
+    auto it = a.begin();
+    EXPECT_EQ(*it, 5);
+    ++it;
+    EXPECT_EQ(*it, 6);
+}
+
+}
